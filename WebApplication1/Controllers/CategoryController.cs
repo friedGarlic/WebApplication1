188296c1616e29c24ca59452cecc46719641d9e6
remcarlmerza <remcarlmerza@gmail.com>
2023-08-23 18:20:36 +0800

    Created, edit and delete function. added pop up notification using toastr extension

--- WebApplication1/Controllers/CategoryController.cs
+++ WebApplication1/Controllers/CategoryController.cs
@@ -11,7 +11,6 @@ namespace WebApplication1.Controllers
         public CategoryController(AppDbContext db)
         {
             _db = db;
-            
         }
         public IActionResult Index()
         {
@@ -19,6 +18,7 @@ namespace WebApplication1.Controllers
             return View(returnDB);
         }
 
+        //------ CREATE FUNCTION
         public IActionResult Create()
         {
             return View();
@@ -32,9 +32,77 @@ namespace WebApplication1.Controllers
             {
                 _db.Categories.Add(category);
                 _db.SaveChanges();
+                TempData["Success"] = "Category Created Successfully";
                 return RedirectToAction("Index");
             }
             else { return View(category); }
+        }
+
+        //------- EDIT FUNCTION
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var foundFromDb = _db.Categories.Find(id);
+
+            if(foundFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(foundFromDb);
+        }
+        //form POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                TempData["Success"] = "Category Saved Successfully";
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        //------- Delete FUNCTION
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var foundFromDb = _db.Categories.Find(id);
+
+            if (foundFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(foundFromDb);
+        }
+        //form POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Category category)
+        {
+            var foundFromDb = _db.Categories.Find(category.Id);
+            
+            if(foundFromDb == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(foundFromDb);
+            _db.SaveChanges();
+            TempData["Success"] = "Category Deleted Successfully";
+
+            return RedirectToAction("Index");
         }
     }
 }
